4510fd56decd29b47225ac4b73146b9783642c33
fhem348 <fhem348@gmail.com>
2024-03-18 20:30:33 +0900

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,26 @@ import { CardModule } from './card/card.module';
 import { BoardModule } from './board/board.module';
 import { ColumnModule } from './column/column.module';
 import { CommentModule } from './comment/comment.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configModuleValidationSchema } from './configs/env-validation.config';
+import { TypeOrmModuleOptions } from './configs/database.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, CardModule, BoardModule, ColumnModule, CommentModule],
+  imports: [
+    UserModule,
+    CardModule,
+    BoardModule,
+    ColumnModule,
+    CommentModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: configModuleValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync(TypeOrmModuleOptions),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,57 @@
-export class User {}
+// import {
+//   IsEmail,
+//   IsEnum,
+//   IsNotEmpty,
+//   IsNumber,
+//   IsString,
+//   IsStrongPassword,
+// } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { UserRole } from '../types/user-role.type';
+import { Board } from 'src/board/entities/board.entity';
+import { Card } from 'src/card/entities/card.entity';
+
+@Entity({
+  name: 'user',
+})
+export class User {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.Customer })
+  role: UserRole;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Board, (Board) => board.user)
+  Board: Board[];
+
+  @OneToMany(() => List, (List) => List.user)
+  List: List[];
+
+  @OneToMany(() => Card, (Card) => Card.user)
+  Card: Card[];
+
+  @OneToMany(() => Comment, (Comment) => Comment.user)
+  Comment: Comment[];
+}
