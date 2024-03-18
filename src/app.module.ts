ee494f69116fb52a128cc39c95b96fc02f9662cf
fhem348 <fhem348@gmail.com>
2024-03-18 21:02:16 +0900

    앱 모듈 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-import Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,34 +5,30 @@ import { UserModule } from './user/user.module';
 import { CardModule } from './card/card.module';
 import { BoardModule } from './board/board.module';
 import { CommentModule } from './comment/comment.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './board/entities/board.entity';
 import { Card } from './card/entities/card.entity';
 import { User } from './user/entities/user.entity';
 import { Comment } from './comment/entities/comment.entity';
 import { ListModule } from './list/list.module';
 import { List } from './list/entities/list.entity';
-
-const typeOrmModuleOptions = {
-  useFactory: async (
-    ConfigService: ConfigService,
-  ): Promise<TypeOrmModuleOptions> => ({
-    type: 'mysql',
-    username: ConfigService.get('DB_USERNAME'),
-    password: ConfigService.get('DB_PASSWORD'),
-    host: ConfigService.get('DB_HOST'),
-    port: ConfigService.get('DB_PORT'),
-    database: ConfigService.get('DB_NAME'),
-    entities: [User, Card, Board, List, Comment],
-    synchronize: ConfigService.get('DB_SYNC'),
-    logging: true,
-  }),
-  inject: [ConfigService],
-};
+import { configModuleValidationSchema } from './configs/env-validation.config';
+import { TypeOrmModuleOptions } from './configs/database.config';
 
 @Module({
-  imports: [UserModule, CardModule, BoardModule, ColumnModule, CommentModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: configModuleValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync(TypeOrmModuleOptions),
+    UserModule,
+    CardModule,
+    BoardModule,
+    ListModule,
+    CommentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
