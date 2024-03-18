85abb9b869fa02c019a8279cb223d3a50452efc8
TriumphOne <woo001232naver.com>
2024-03-18 17:36:36 +0900

    Feat: card entity

--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,1 +1,20 @@
-export class Comment {}
+import { Card } from 'src/card/entities/card.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'comments',
+})
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', select: true, nullable: true })
+  comment: string;
+
+  @ManyToOne(() => User, (user) => user.comment)
+  user: User[];
+
+  @ManyToOne(() => Card, (card) => card.comment)
+  card: Card[];
+}
