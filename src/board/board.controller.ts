19186612e81a5bb32e909235c7ab450118490343
semin <a82727282@gmail.com>
2024-03-20 01:32:09 +0900

    fix: api param

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -14,33 +14,33 @@ import { UpdateBoardDto } from './dto/update-board.dto';
 @Controller('boards') // userId 필요
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
-
+  // board 생성
   @Post()
   createBoard(@Body() createBoardDto: CreateBoardDto) {
     return this.boardService.create(createBoardDto);
   }
-
+  // 내가 생성한 board 조회
   @Get()
   async findAll() {
     return await this.boardService.findAll(userId);
   }
-
+  // 내가 참여하는 board 조회
   @Get()
   async findAllMyTeamProject() {
     return await this.boardService.findAllMyTeamProject(userId);
   }
-
+  // board 상세조회
   @Get(':boardId')
   findOne(@Param('boardId') boardId: number) {
     return this.boardService.findOne(boardId);
   }
-
-  @Patch(':id')
+  // board 수정
+  @Patch(':boardId')
   update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
     return this.boardService.update(+id, updateBoardDto);
   }
-
-  @Delete(':id')
+  // board 삭제
+  @Delete(':boardId')
   remove(@Param('id') id: string) {
     return this.boardService.remove(+id);
   }
--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -4,9 +4,10 @@ import { BoardController } from './board.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Member } from 'src/member/entities/member.entity';
+import { Board } from './entities/board.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Member])],
+  imports: [TypeOrmModule.forFeature([User, Member, Board])],
   controllers: [BoardController],
   providers: [BoardService],
 })
