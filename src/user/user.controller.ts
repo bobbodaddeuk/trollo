5980ed8a78c3e4906c6a4a28b4775baa5ef49b33
fhem348 <fhem348@gmail.com>
2024-03-20 22:18:21 +0900

    정보수정 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,20 @@
 import {
+  Body,
   Controller,
   Get,
   HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
   Request,
   UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { userInfo } from 'src/utils/userInfo.decorator';
+import { User } from './entities/user.entity';
 
 @ApiTags('사용자')
 @Controller('users')
@@ -17,15 +24,30 @@ export class UserController {
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Get('/me')
-  async findMe(@Request() req) {
-    const userId = req.user.id;
-
-    const data = await this.userService.findOneById(userId);
+  async findMe(@userInfo() user: User) {
+    const data = await this.userService.findOneById(user);
 
     return {
       statusCode: HttpStatus.OK,
       message: '내 정보 조회에 성공했습니다.',
       data,
+    };
+  }
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Patch('/me')
+  async updateUser(
+    @Body() updateUserDto: UpdateUserDto,
+    @userInfo() user: User,
+  ) {
+    const updatedUser = await this.userService.updateUser(
+      user.id,
+      updateUserDto,
+    );
+    return {
+      statusCode: HttpStatus.OK,
+      message: '내 정보 수정에 성공했습니다.',
+      data: updatedUser,
     };
   }
 }
