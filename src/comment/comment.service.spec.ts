9fc7cde7067d3548efa4231c81c2ddbf8466d58e
TriumphOne <woo001232naver.com>
2024-03-25 16:41:24 +0900

    Feat: 댓글 서비스 테스트 코드 작성 - 되진 않음

--- src/comment/comment.service.spec.ts
+++ src/comment/comment.service.spec.ts
@@ -1,29 +1,111 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentService } from './comment.service';
-import { Repository } from 'typeorm';
-
-const mockRepository = () => ({
-  findOneBy: jest.fn(),
-  find: jest.fn(),
-  create: jest.fn(),
-  save: jest.fn(),
-  delete: jest.fn(),
-});
-
-type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { Card } from '../card/entities/card.entity';
+import { List } from '../list/entities/list.entity';
+import { Board } from '../board/entities/board.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('CommentService', () => {
   let service: CommentService;
+  let mockCommentRepository,
+    mockCardRepository,
+    mockListRepository,
+    mockBoardRepository;
 
   beforeEach(async () => {
+    mockCommentRepository = {
+      findOneBy: jest.fn(),
+      find: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+      delete: jest.fn(),
+    };
+
+    mockCardRepository = {
+      findOneBy: jest.fn(),
+    };
+
+    mockListRepository = {
+      findOneBy: jest.fn(),
+    };
+
+    mockBoardRepository = {
+      findOneBy: jest.fn(),
+    };
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommentService],
+      providers: [
+        CommentService,
+        {
+          provide: getRepositoryToken(Comment),
+          useValue: mockCommentRepository,
+        },
+        {
+          provide: getRepositoryToken(Card),
+          useValue: mockCardRepository,
+        },
+        {
+          provide: getRepositoryToken(List),
+          useValue: mockListRepository,
+        },
+        {
+          provide: getRepositoryToken(Board),
+          useValue: mockBoardRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<CommentService>(CommentService);
   });
+  describe('댓글 생성 테스트', () => {
+    it('성공적인 댓글 생성', async () => {
+      const sampleComment = {
+        boardId: 1,
+        listId: 2,
+        cardId: 3,
+        createCommentDto: { content: '테스트' },
+      };
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+      mockBoardRepository.findOneBy.mockResolvedValue(null);
+      mockListRepository.findOneBy.mockResolvedValue(null);
+      mockCardRepository.findOneBy.mockResolvedValue(null);
+
+      await expect(
+        service.createComment(
+          sampleComment.boardId,
+          sampleComment.listId,
+          sampleComment.cardId,
+          sampleComment.createCommentDto,
+        ),
+      ).rejects.toThrow(NotFoundException);
+
+      mockCommentRepository.create.mockReturnValue(
+        sampleComment.createCommentDto,
+      );
+      mockCommentRepository.save.mockResolvedValue(
+        sampleComment.createCommentDto,
+      );
+
+      const result = await service.createComment(
+        sampleComment.boardId,
+        sampleComment.listId,
+        sampleComment.cardId,
+        sampleComment.createCommentDto,
+      );
+      expect(result).toEqual(sampleComment.createCommentDto);
+      expect(mockCommentRepository.create).toHaveBeenCalledWith(
+        sampleComment.createCommentDto,
+      );
+      expect(mockCommentRepository.create).toHaveBeenCalledTimes(1);
+      expect(mockCommentRepository.save).toHaveBeenCalledWith(
+        sampleComment.createCommentDto,
+      );
+      expect(mockCommentRepository.save).toHaveBeenCalledTimes(1);
+    });
   });
+  // describe('댓글 조회 테스트', () => {
+
+  // })
 });
